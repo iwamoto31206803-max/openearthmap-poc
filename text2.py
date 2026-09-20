9a7617f7101ca0489cb1f0be4d85aed572500df6
iwamoto31206803-max <iwamoto31206803@gmail.com>
2026-09-20 14:22:19 +0900

    Update text2.py

--- text2.py
+++ text2.py
@@ -1,5 +1,3 @@
 cd /d C:\OpenEarthMap_PoC\openearthmap-poc
 
-dir /s /b training_outputs\*20260917T160534_801449Z_8c785b89*\best.pth
-dir /s /b training_outputs\*20260919T053943_488504Z_000833f3*\best.pth
-dir /s /b training_outputs\*20260919T151035_437811Z_0fd54ade*\best.pth
+dir /s /b training_outputs\best.pth
