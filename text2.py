97d7afb2eef82a1d3c0d717dea0c9b56bfec721f
iwamoto31206803-max <iwamoto31206803@gmail.com>
2026-09-20 20:04:33 +0900

    Update text2.py

--- text2.py
+++ text2.py
@@ -1,4 +1,7 @@
-cd /d C:\OpenEarthMap_PoC\OpenEarthMap-SAR
+cd /d C:\OpenEarthMap_PoC
 
-echo ===== Semantic Segmentation dataset =====
-dir /s src\Semantic_Segemtation\dataset
+mkdir oemsar_data 2>nul
+cd oemsar_data
+
+curl -L -C - -o dfc25_track1_trainval.zip ^
+"https://zenodo.org/records/14622048/files/dfc25_track1_trainval.zip?download=1"
