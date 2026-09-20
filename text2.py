f1c2cf6373a9ffe294704d971132decce1a65269
iwamoto31206803-max <iwamoto31206803@gmail.com>
2026-09-20 14:19:43 +0900

    Update text2.py

--- text2.py
+++ text2.py
@@ -1,7 +1,5 @@
-python compare_base_ft.py ^
-  --lat 35.662 ^
-  --lon 140.070 ^
-  --name chiba01 ^
-  --base-model C:\OpenEarthMap_PoC\OpenEarthMap-SAR\src\Semantic_Segemtation\pretrained\RGB_Real_5_u-efficientnet-b4.pth ^
-  --fine-tuned-model C:\OpenEarthMap_PoC\openearthmap-poc\training_outputs\gsi_phase_b_v01\20260919T151035_437811Z_0fd54ade\checkpoints\best.pth ^
-  --output-dir C:\OpenEarthMap_PoC\comparison\phase_b_v03_road
+cd /d C:\OpenEarthMap_PoC\openearthmap-poc
+
+dir /s /b training_outputs\*20260917T160534_801449Z_8c785b89*\best.pth
+dir /s /b training_outputs\*20260919T053943_488504Z_000833f3*\best.pth
+dir /s /b training_outputs\*20260919T151035_437811Z_0fd54ade*\best.pth
