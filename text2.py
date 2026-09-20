58e43d7212b6d5299738b9038d599c12730abd08
iwamoto31206803-max <iwamoto31206803@gmail.com>
2026-09-20 19:45:50 +0900

    Update text2.py

--- text2.py
+++ text2.py
@@ -1,22 +1,23 @@
-cd /d C:\OpenEarthMap_PoC
+cd /d C:\OpenEarthMap_PoC\OpenEarthMap-SAR
 
-echo ===== 1. RGB_Real_5 checkpoint =====
-dir /s /b RGB_Real_5_u-efficientnet-b4.pth 2>nul
-
-echo.
-echo ===== 2. OEM-SARらしいフォルダ =====
-dir /s /b /ad *OpenEarthMap* 2>nul
-dir /s /b /ad *OEM*SAR* 2>nul
+echo ===== root =====
+dir
 
 echo.
-echo ===== 3. train / val / labels / rgb_images folders =====
-dir /s /b /ad train 2>nul
-dir /s /b /ad val 2>nul
-dir /s /b /ad labels 2>nul
-dir /s /b /ad rgb_images 2>nul
-dir /s /b /ad sar_images 2>nul
+echo ===== dataset folders =====
+dir /s /b /ad dataset 2>nul
+dir /s /b /ad data 2>nul
 
 echo.
-echo ===== 4. TrainArea / ValArea files =====
-dir /s /b *TrainArea*.tif 2>nul
-dir /s /b *ValArea*.tif 2>nul
+echo ===== tif count =====
+dir /s /b *.tif 2>nul | find /c /v ""
+
+echo.
+echo ===== zip / tar / 7z =====
+dir /s /b *.zip 2>nul
+dir /s /b *.tar 2>nul
+dir /s /b *.gz 2>nul
+dir /s /b *.7z 2>nul
+
+
+certutil -hashfile "C:\OpenEarthMap_PoC\OpenEarthMap-SAR\src\Semantic_Segemtation\pretrained\RGB_Real_5_u-efficientnet-b4.pth" SHA256
