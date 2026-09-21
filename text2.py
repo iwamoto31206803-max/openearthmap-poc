faf9e876d87007e3477c87d8efe173fa6d7ccd85
iwamoto31206803-max <iwamoto31206803@gmail.com>
2026-09-21 12:43:51 +0900

    Update text2.py

--- text2.py
+++ text2.py
@@ -1,26 +1,16 @@
-cd /d C:\OpenEarthMap_PoC\oemsar_data\trainval
+cd /d C:\OpenEarthMap_PoC\oemsar_data
 
-echo ===== COUNTS =====
-echo train Labels:
-dir /b train\Labels\*.tif | find /c /v ""
+curl -L -o dfc25_track1_val_labels.zip ^
+"https://zenodo.org/records/14950559/files/dfc25_track1_val_labels.zip?download=1"
 
-echo train RGB:
-dir /b train\rgb_images\*.tif | find /c /v ""
+certutil -hashfile dfc25_track1_val_labels.zip MD5
 
-echo train SAR:
-dir /b train\sar_images\*.tif | find /c /v ""
+powershell -NoProfile -Command "Expand-Archive -Path 'C:\OpenEarthMap_PoC\oemsar_data\dfc25_track1_val_labels.zip' -DestinationPath 'C:\OpenEarthMap_PoC\oemsar_data\val_labels' -Force"
 
-echo val SAR:
-dir /b val\sar_images\*.tif | find /c /v ""
+echo ===== VAL LABELS =====
+dir /s /b C:\OpenEarthMap_PoC\oemsar_data\val_labels\*.tif | find /c /v ""
 
 echo.
-echo ===== FIRST 20 LABELS =====
-dir /b train\Labels\*.tif | more
-
-echo.
-echo ===== FIRST 20 RGB =====
-dir /b train\rgb_images\*.tif | more
-
-echo.
-echo ===== FIRST 20 SAR =====
-dir /b train\sar_images\*.tif | more
+echo ===== FIRST 20 =====
+dir /s /b C:\OpenEarthMap_PoC\oemsar_data\val_labels\*.tif | more
+  
